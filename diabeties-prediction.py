131848b2759bfd02aaab6039afeae9e9bc9c17f7
itsraghavsharma <raghav.sharma643@gmail.com>
2023-03-09 08:53:51 +0530

    Model started predicting

--- diabeties-prediction.py
+++ diabeties-prediction.py
@@ -18,3 +18,77 @@ print("\nRows & Colums = "+str(diabetes_dataset.shape))
 
 print("\n\ngetting the statistical measures of the data\n\n")
 print(diabetes_dataset.describe())
+
+print("\n\n")
+print(diabetes_dataset['Outcome'].value_counts())
+print("\n\n")
+print(diabetes_dataset.groupby('Outcome').mean())
+print("\n\n")
+
+print("separating the data and labels")
+X = diabetes_dataset.drop(columns = 'Outcome', axis=1)
+Y = diabetes_dataset['Outcome']
+
+print(X)
+print("\n\n")
+print(Y)
+print("\n\n")
+
+print("Data Standardization")
+scaler = StandardScaler()
+scaler.fit(X)
+standardized_data = scaler.transform(X)
+print(standardized_data)
+
+X = standardized_data
+Y = diabetes_dataset['Outcome']
+
+print(X)
+print(Y)
+print("\n\n")
+
+print("Train Test Split")
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
+
+print(X.shape, X_train.shape, X_test.shape)
+print("\n\n")
+
+print("Training the Model")
+classifier = svm.SVC(kernel='linear')
+
+#training the support vector Machine Classifier
+classifier.fit(X_train, Y_train)
+
+# accuracy score on the training data
+X_train_prediction = classifier.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+
+print('Accuracy score of the training data : ', training_data_accuracy)
+
+# accuracy score on the test data
+X_test_prediction = classifier.predict(X_test)
+test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+
+print('Accuracy score of the test data : ', test_data_accuracy)
+
+input_data = (5,166,72,19,175,25.8,0.587,51)
+
+# changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+# standardize the input data
+std_data = scaler.transform(input_data_reshaped)
+# print(std_data)
+
+prediction = classifier.predict(std_data)
+print(prediction)
+
+
+if (prediction[0] == 0):
+  print('The person is not diabetic')
+else:
+  print('The person is diabetic')
